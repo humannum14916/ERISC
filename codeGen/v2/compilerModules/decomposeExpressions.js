eaabb2f9837e2d6693fbd03231a0ddfadccfa35f
humannum14916 <humannum14916@gmail.com>
2022-01-21 21:19:12 +0000

    Replace ! with ~

--- codeGen/v2/compilerModules/decomposeExpressions.js
+++ codeGen/v2/compilerModules/decomposeExpressions.js
@@ -60,17 +60,17 @@ function dExpression(e){
     }
     return o;
   })(e);
-  //!
+  //~
   e = (e=>{
     let o = [];
     while(e.length != 0){
       let cur = e.shift();
-      if(cur.type == "operator" && cur.value.value == "!"){
+      if(cur.type == "operator" && cur.value.value == "~"){
         //get value to invert
         let val = e.shift();
-        if(!val) misc.error("cannot end and expression with !",cur);
+        if(!val) misc.error("cannot end and expression with ~",cur);
         //add
-        o.push({type:"!",a:val});
+        o.push({type:"~",a:val});
       } else {
         o.push(cur);
       }
