8ebddd9612a77341afc0be89422dee03e0aa4498
humannum14916 <humannum14916@gmail.com>
2022-01-24 18:28:49 +0000

    Use console.error instead of console.log for build scripts

--- os/v3/build.js
+++ os/v3/build.js
@@ -3,16 +3,16 @@ const {call} = require("../../utils/ioWrap.js");
 let source = "os/v3";//process.argv[2];
 let dest = "images/v3";//process.argv[3];
 
-console.log("Building...");
+console.error("Building...");
 //compile boot ROM
-console.log("Assembling boot ROM...");
+console.error("Assembling boot ROM...");
 call("codeGen/v2/assembler.js",null,[
   "-i",source+"/system/bootRom.txt",
   "-o",dest+"/bootRom",source+"/"
 ]);
-console.log("Boot ROM assembled");
+console.error("Boot ROM assembled");
 //compile filesystem
-console.log("Building filesystem...");
+console.error("Building filesystem...");
 call("codeGen/v2/FSgen.js","null",["-o",dest+"/disk",source+"/",dest]);
-console.log("Filesystem built");
-console.log("OS built");
+console.error("Filesystem built");
+console.error("OS built");
